811d923d0992a833013e9bd3de93d3e03f8dc8aa
MiHu773 <michal@tag.waw.pl>
2018-05-26 20:16:34 +0200

    Receiving stroke and beginning of sending

--- src/main/java/drawie/Model.java
+++ src/main/java/drawie/Model.java
@@ -2,6 +2,7 @@ package drawie;
 
 import io.socket.client.IO;
 import io.socket.client.Socket;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -35,6 +36,17 @@ public class Model {
             }
             roomController.drawDumpBCOnCanvas(imgInB64[imgInB64.length-1]);
         });
+
+        socket.on("strokeBC", args -> {
+            JSONObject obj = (JSONObject) args[0];
+            System.out.println(obj.toString());
+            try {
+                JSONObject opt = obj.getJSONObject("options");
+                roomController.drawStrokeBCOnCanvas(opt.getString("strokeStyle"), opt.getString("lineCap"), opt.getString("fillStyle"), opt.getInt("lineWidth"), obj.getJSONArray("stroke"));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        });
     }
 
     public void handleRedoClick() {
@@ -43,6 +55,23 @@ public class Model {
 
     public void handleUndoClick() {
         socket.emit("undo");
+    }
+
+    public void sendStroke(String color, String lineCap, String fillStyle, int lineWidth, JSONArray stroke) {
+        JSONObject strokeObj = new JSONObject();
+        JSONObject options = new JSONObject();
+        try {
+            options.put("strokeStyle", color);
+            options.put("lineCap", lineCap);
+            options.put("fillStyle", fillStyle);
+            options.put("lineWidth", lineWidth);
+            strokeObj.put("options", options);
+            strokeObj.put("stroke", stroke);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        socket.emit("stroke", strokeObj);
     }
 
     public void newRoom() { }
--- src/main/java/drawie/RoomController.java
+++ src/main/java/drawie/RoomController.java
@@ -6,6 +6,10 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Slider;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.StrokeLineCap;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import sun.misc.BASE64Decoder;
 
 import java.io.ByteArrayInputStream;
@@ -15,6 +19,8 @@ public class RoomController {
 
     private View view;
     private Model model;
+
+    private JSONArray mStroke;
 
     @FXML
     private Canvas roomCanvas;
@@ -33,14 +39,24 @@ public class RoomController {
         GraphicsContext gc = roomCanvas.getGraphicsContext2D();
         try {
             roomCanvas.setOnMousePressed(event -> {
+                mStroke = new JSONArray();
+//                mStroke.put(new JSONArray().put((int) event.getX()).put((int) event.getX())); TODO dodawanie tego JSONA
                 gc.beginPath();
                 gc.lineTo(event.getX(), event.getY());
                 gc.stroke();
             });
 
             roomCanvas.setOnMouseDragged(event -> {
+                /*JSONArray points = new JSONArray(); //TODO WRZUCANIE DO TABLICY
+                points.put((int) event.getX());
+                points.put((int) event.getX());
+                mStroke.put(points);*/
                 gc.lineTo(event.getX(), event.getY());
                 gc.stroke();
+            });
+
+            roomCanvas.setOnMouseReleased(event ->{
+                model.sendStroke("#00ff00", "round", "solid", (int) gc.getLineWidth(), mStroke);
             });
         } catch (Exception e) {
             System.out.println(e);
@@ -84,6 +100,46 @@ public class RoomController {
         GraphicsContext gc = roomCanvas.getGraphicsContext2D();
         gc.clearRect(0, 0, roomCanvas.getWidth(), roomCanvas.getHeight());
         gc.drawImage(dumpImg, 0, 0);
-        System.out.println("Image drawn");
+    }
+
+    public void drawStrokeBCOnCanvas(String color, String lineCap, String fillStyle, int lineWidth, JSONArray stroke){
+        GraphicsContext gc = roomCanvas.getGraphicsContext2D();
+        gc.setStroke(new Color(
+                (float)Integer.valueOf( color.substring( 1, 3 ), 16 )/256f,
+                (float)Integer.valueOf( color.substring( 3, 5 ), 16 )/256f,
+                (float)Integer.valueOf( color.substring( 5, 7 ), 16 )/256f, 1));
+
+
+        StrokeLineCap slc;
+        switch (lineCap){
+            case "round":
+                slc = StrokeLineCap.ROUND;
+                break;
+            case "square":
+                slc = StrokeLineCap.SQUARE;
+            default:
+                slc = StrokeLineCap.BUTT;
+        }
+        gc.setLineCap(slc);
+
+        //TODO setFillStyle?
+
+        gc.setLineWidth(lineWidth);
+        //drawStroke
+        gc.beginPath();
+        for (int i=0; i<stroke.length(); i++){
+            JSONArray points;
+            int x=0,y=0;
+            try {
+                points = stroke.getJSONArray(i);
+                x = points.getInt(0);
+                y = points.getInt(1);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            gc.lineTo(x, y);
+            gc.stroke();
+        }
     }
 }
